5dc9b5f0e522408e375cfdb2c898c18918fb82c8
Wayne <15210876095@163.com>
2016-05-14 15:58:33 +0800

    blank project

---
+++ MyCoolCodeReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCoolCodeReader
+//
+//  Created by Wayne on 16/5/14.
+//  Copyright © 2016年 WayneHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCoolCodeReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCoolCodeReader
+//
+//  Created by Wayne on 16/5/14.
+//  Copyright © 2016年 WayneHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
